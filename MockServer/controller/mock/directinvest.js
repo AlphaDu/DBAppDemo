dc128b9a0d101173fce2c8de3a003a006b193f07
yfdu <yfdu@cupdata.com>
2017-05-25 11:04:46 +0800

    [master]

--- MockServer/controller/mock/directinvest.js
+++ MockServer/controller/mock/directinvest.js
@@ -1,3 +1,68 @@
-expoors.POST = function (req,res,next) {
+var Mock = require('mockjs');
+function mockVestList (req, res, next) {
+    // /url?page=2&percount=10
+    var TOTAL_COUNT=193;
+    var PER_COUNT=10;
+    var responseData={};
 
-};
+    function idgenerator (orderNum) {
+        return parseInt(parseInt(Math.random() * 100000).toString()  + orderNum);
+    }
+    var template = {
+        label:Mock.Random.cword(5),
+        progress:parseInt(Math.random()*100)*1000,
+        remain:Math.random()*1000 ,
+        limit: 360,
+        earning: parseFloat(63.40847086040856).toFixed(2) + '%'
+    };
+    var page=parseInt(req.query.page);
+    var per=parseInt(req.query.per);
+    var remainNum=TOTAL_COUNT - (page - 1) * per;
+    if (remainNum <= 0){
+
+        res.send({
+            attr: 1,
+            msg: '没有数据',
+            hasNext: false,
+            data:{}
+        })
+    } else if (remainNum <= per){
+        let data=[];
+        for (var i=0; i < remainNum; i++) {
+            data.push(Object.assign({id:idgenerator((page - 1) * per + i)},{
+                label:Mock.Random.cword(5),
+                progress:parseInt(Math.random()*100) + '%',
+                remain:parseFloat(Math.random()*1000).toFixed(2),
+                limit: 360,
+                earning: parseFloat(63.40847086040856).toFixed(2) + '%'
+            }));
+
+        }
+        res.send({
+            attr: 1,
+            msg: '操作成功',
+            hasNext: false,
+            data:data
+        })
+    } else {
+        let data=[];
+        for (var j = 0 ;j < per;j ++){
+
+            data.push(Object.assign({id:idgenerator((page - 1) * per + j)},{
+                label:Mock.Random.cword(5),
+                progress:parseInt(Math.random()*100)*1000,
+                remain:Math.random()*1000 ,
+                limit: 360,
+                earning: parseFloat(63.40847086040856).toFixed(2) + '%'
+            }))
+        }
+        res.send({
+            attr: 1,
+            msg: '操作成功',
+            hasNext: true,
+            data:data
+        })
+    }
+}
+exports.POST=mockVestList;
+exports.GET=mockVestList;
--- ReactNativeApp/app/mobx/InvestListStore.js
+++ ReactNativeApp/app/mobx/InvestListStore.js
@@ -1,51 +1,34 @@
 import {observable, computed, action, runInAction} from 'mobx'
-const REQUEST_URL="";
-class InvestListStore {
-    @observable listData={};
+import HttpStore from './CommonHttpStore'
+const REQUEST_URL="http://172.20.10.4:900";
+const PER_PAGE = 10 ;
+class InvestListStore extends HttpStore{
+    @observable listData=[];
     //0:缺省，1:刷新中，2:成功，-1：失败
-    @observable status=0;
+    @observable isFetching=false;
     @observable msg='';
     @observable page=1;
     @observable isRefreshing=false;
-
     @action
     fetchInvestList=async () => {
+        const URL = REQUEST_URL + "?page=" +"&page=" + PER_PAGE;
         try {
-            const result=await this._fetchDataFromUrl();
+            this.isFetching=true;
+            const result=await this._fetchDataFromUrl(URL);
             runInAction(() => {
-                this.status=2;
                 this.isRefreshing=false;
                 this.msg='';
                 if (this.page == 1){
-                    this.listData=result.slice(0);
+                    this.listData=result.data.slice(0);
                 } else {
-                    this.listData.concat(result)
+                    this.listData.concat(result.data)
                 }
+                this.isFetching=false;
             });
         } catch (error) {
-            this.status= -1;
+            this.isFetching=false;
             this.msg=error;
         }
     };
-
-    _fetchDataFromUrl (url) {
-        return new Promise((resolve, reject) => {
-            fetch(url)
-                .then(response => {
-                    if (reqponse.status == 200) return response.json();
-                    reject('非200错误');
-                })
-                .then(responseData=>{
-                    if (responseData) {
-                        resolve(responseData.feeds)
-                    } else {
-                        reject('数据错误！')
-                    }
-                })
-                .catch(error =>{
-                    reject('网络出错！')
-                });
-        });
-    }
 }
 export default  InvestListStore;
